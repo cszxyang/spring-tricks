3c64b1537181402f5aeae74811fd04fb06cc5393
cszxyang <linfengit@qq.com>
2019-12-22 21:56:08 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#### 与 Spring 相关的一些实验
+
+- 如何在 Spring 中优雅地使用责任链设计模式：spring-CoR
+- Spring 事务传播属性实验
+- Spring 统一异常处理
+- Spring AoP 日志切面
--- spring-CoR/src/main/java/com/github/cszxyang/handler/BaseHandler.java
+++ spring-CoR/src/main/java/com/github/cszxyang/handler/BaseHandler.java
@@ -2,6 +2,12 @@ package com.github.cszxyang.handler;
 
 import com.github.cszxyang.request.ServiceReq;
 
+/**
+ * 通用处理器
+ *
+ * @author cszxyang
+ * @since 2019/12/22
+ */
 public abstract class BaseHandler {
     abstract void process(ServiceReq serviceReq);
 }
